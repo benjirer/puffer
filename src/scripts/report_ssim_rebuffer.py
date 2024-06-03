be5aae29ca458289a7f5d310e4c29429f7e24f05
benjirer <80070426+benjirer@users.noreply.github.com>
2024-06-03 08:28:40 +0200

    revert to original player

--- src/scripts/report_ssim_rebuffer.py
+++ src/scripts/report_ssim_rebuffer.py
@@ -9,8 +9,8 @@ from datetime import datetime, timedelta
 from subprocess import check_call
 
 
-time_str = '%Y-%m-%dT%H:%M:%SZ'
-short_time_str = '%Y-%m-%d'
+time_str = "%Y-%m-%dT%H:%M:%SZ"
+short_time_str = "%Y-%m-%d"
 args = None
 
 
@@ -18,61 +18,72 @@ def report_ssim_rebuffer(curr_ts, days):
     start_ts = curr_ts - timedelta(days=days)
 
     curr_dir = path.dirname(path.abspath(__file__))
-    plot_src = path.join(curr_dir, 'plot_ssim_rebuffer.py')
+    plot_src = path.join(curr_dir, "plot_ssim_rebuffer.py")
 
-    time_range = '{}_{}'.format(start_ts.strftime(short_time_str),
-                                curr_ts.strftime(short_time_str))
-    output_fig_name = time_range + '.png'
+    time_range = "{}_{}".format(
+        start_ts.strftime(short_time_str), curr_ts.strftime(short_time_str)
+    )
+    output_fig_name = time_range + ".png"
     output_fig = path.join(curr_dir, output_fig_name)
 
     # run plot_ssim_rebuffer.py
-    cmd = [plot_src, args.yaml_settings, '-o', output_fig,
-           '--from', start_ts.strftime(time_str),
-           '--to', curr_ts.strftime(time_str)]
-    sys.stderr.write(' '.join(cmd) + '\n')
+    cmd = [
+        plot_src,
+        args.yaml_settings,
+        "-o",
+        output_fig,
+        "--from",
+        start_ts.strftime(time_str),
+        "--to",
+        curr_ts.strftime(time_str),
+    ]
+    sys.stderr.write(" ".join(cmd) + "\n")
 
     check_call(cmd)
 
     # upload output_fig to Google cloud storage
-    bucket_folder = 'puffer-stanford-public/ssim-rebuffer-figs'
-    cmd = 'gsutil cp {} gs://{}'.format(output_fig, bucket_folder)
-    sys.stderr.write(cmd + '\n')
+    bucket_folder = "puffer-stanford-public/ssim-rebuffer-figs"
+    cmd = "gsutil cp {} gs://{}".format(output_fig, bucket_folder)
+    sys.stderr.write(cmd + "\n")
     check_call(cmd, shell=True)
-    gs_url = ('https://storage.googleapis.com/{}/{}'
-              .format(bucket_folder, output_fig_name))
+    gs_url = "https://storage.googleapis.com/{}/{}".format(
+        bucket_folder, output_fig_name
+    )
 
     # remove local output_fig
     os.remove(output_fig)
 
     # post output_fig to Zulip
-    template = 'Performance of ongoing experiments over the past {}:\n' + gs_url
+    template = "Performance of ongoing experiments over the past {}:\n" + gs_url
     if days == 1:
-        content = template.format('day')
+        content = template.format("day")
     elif days == 7:
-        content = template.format('week')
+        content = template.format("week")
     elif days == 14:
-        content = template.format('two weeks')
+        content = template.format("two weeks")
     else:
-        content = template.format('{} days'.format(days))
+        content = template.format("{} days".format(days))
 
     payload = [
-        ('type', 'stream'),
-        ('to', 'puffer-notification'),
-        ('subject', 'Daily Report'),
-        ('content', content),
+        ("type", "stream"),
+        ("to", "puffer-notification"),
+        ("subject", "Daily Report"),
+        ("content", content),
     ]
     response = requests.post(
-        os.environ['ZULIP_URL'], data=payload,
-        auth=(os.environ['ZULIP_BOT_EMAIL'], os.environ['ZULIP_BOT_TOKEN']))
+        os.environ["ZULIP_URL"],
+        data=payload,
+        auth=(os.environ["ZULIP_BOT_EMAIL"], os.environ["ZULIP_BOT_TOKEN"]),
+    )
     if response.status_code == requests.codes.ok:
-        print('Posted to Zulip successfully')
+        print("Posted to Zulip successfully")
     else:
-        print('Failed to post to Zulip')
+        print("Failed to post to Zulip")
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('yaml_settings')
+    parser.add_argument("yaml_settings")
     global args
     args = parser.parse_args()
 
@@ -89,5 +100,5 @@ def main():
     report_ssim_rebuffer(curr_ts, 14)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
